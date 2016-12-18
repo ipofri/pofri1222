5cf3fbdf5d2c51150dc89bf8b51f2becc1e5cb73
ipofri <pofri1222@naver.com>
2016-12-18 14:32:31 +0900

    ddd

--- PRO.3LineTile.Jungol.2112.161211/PRO.3LineTile.Jungol.2112.161211.cpp
+++ PRO.3LineTile.Jungol.2112.161211/PRO.3LineTile.Jungol.2112.161211.cpp
@@ -1,19 +1,55 @@
 #include <stdio.h>
 
 int N;
-int DP[40];
+int DP[40][3];
 
-int Solve(int n)
+int Solve(int n,int step)
 {
-	if (n == 4)
-		return 11;
-	
-	int ret = Solve(n-4)*11
-	return 0;
+	if (DP[n][step] != -1)
+		return DP[n][step];
+
+	int ret = 0;
+
+	if (step == 0)
+	{
+		ret = Solve(n - 2, 0) * 3 + Solve(n - 2, 1) + Solve(n - 2, 2);
+	}
+	else if (step == 1)
+	{
+		ret = Solve(n - 2, 0) + Solve(n - 2, 1);
+	}
+	else if (step == 2)
+	{
+		ret = Solve(n - 2, 0) + Solve(n - 2, 2);
+	}
+
+	DP[n][step] = ret;
+
+	return ret;
 }
+
 int main(void)
 {
 	scanf("%d", &N);
-	printf("%d", Solve(N));
+
+	for (int i = 2; i < 40; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			DP[i][j] = -1;
+		}
+	}
+
+	DP[2][0] = 3;
+	DP[2][1] = 1;
+	DP[2][2] = 1;
+
+	int ret = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		Solve(N, i);
+	}
+
+	printf("%d", DP[N][0]);
 	return 0;
 }
